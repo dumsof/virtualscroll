00be1eed48a53bef2259f351e4398ad5b9dfbb6b
Darwin <darwinu34@yahoo.es>
2019-09-03 15:21:03 -0500

    se finaliza el drag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,19 @@ import { VirtualComponent } from './virtual/virtual.component';
 
 /* poder utilizar el virtual scroll */
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragComponent } from './drag/drag.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VirtualComponent
+    VirtualComponent,
+    DragComponent
   ],
   imports: [
     BrowserModule,
-    ScrollingModule
+    ScrollingModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
